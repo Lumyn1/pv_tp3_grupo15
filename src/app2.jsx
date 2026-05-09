97fffd69feecca04b8460b307326b57a32801aca
Lumyn1 <lucianomartin2002@hotmail.com>
2026-05-09 15:06:29 -0300

    primer commit proyecto react TP3 Riveros Luciano

---
+++ src/app2.jsx
@@ -0,0 +1,16 @@
+
+
+const App2 = () =>{ 
+
+     console.log ("hola mundo");
+
+     return (
+        <div>
+        <h1>Luciano Martín Riveros</h1>
+        <Usuarios />
+        </div>
+     )    
+
+}
+
+export default App2;
