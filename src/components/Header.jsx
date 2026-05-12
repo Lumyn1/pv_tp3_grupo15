6edfab5dd7a0ae9777445e5c6b670d5bce9c8f2e
TorresNahuelTobias <tobiastorres123456@gmail.com>
2026-05-12 20:44:12 -0300

    subo parte del Layout, el heder, nav y su correspondiente css @Torres Nahuel Tobias

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+
+    return (
+
+        <header>
+
+            <h1>Gestión de Proyectos Educativos</h1>
+
+            <p>
+                Plataforma para visualizar proyectos realizados por estudiantes.
+            </p>
+
+        </header>
+
+    );
+};
+
+export default Header;
