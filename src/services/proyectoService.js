ff0799639bf996fe89bd636fba43612aa27cf9d1
Lumyn1 <lucianomartin2002@hotmail.com>
2026-05-12 21:39:13 -0300

    corregidos errores Luciano Riveros

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -10,7 +10,7 @@ const proyectoService = (() => {
     ];
 
     // Obtener copia del arreglo
-    const listarProyecto = () => {
+    const listarProyectos = () => {
         return [...proyecto];
     };
 
@@ -51,7 +51,7 @@ const proyectoService = (() => {
     };
 
     return {
-        listarProyecto,
+        listarProyectos,
         buscarProyecto,
         agregarProyecto,
         eliminarProyecto
