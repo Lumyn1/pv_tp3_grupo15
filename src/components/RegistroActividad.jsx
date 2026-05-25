250152dcf1f95a1a4becab9ae156e84ec917e0a1
TorresNahuelTobias <tobiastorres123456@gmail.com>
2026-05-25 20:07:54 -0300

    subo el punto 3-Formato del Mensaje  @Torres Nahuel Tobias

--- src/components/RegistroActividad.jsx
+++ src/components/RegistroActividad.jsx
@@ -1,13 +1,28 @@
 const RegistroActividad = ({ fecha }) => {
+
+  const fechaFormateada = new Date(fecha).toLocaleDateString();
+
+  const horaFormateada = new Date(fecha).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
   return (
-    <div className="registro-actividad">
+ <div className="registro-actividad">
+
       <h3>Registro de Actividad</h3>
 
       <p>
-        Última modificación: {fecha}
+        Última actualización de la lista:
+        {" "}
+        {fechaFormateada}
+        {" "}a las{" "}
+        {horaFormateada}
+        hs.
       </p>
+
     </div>
   );
-};
+  };
 
 export default RegistroActividad;
